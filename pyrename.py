6953f4f1dee6272d6b45c734dfb45c515d625542
LUO, Shouxi <rithmns@gmail.com>
2013-08-07 16:25:16 +0800

    Create pyrename.py

---
+++ pyrename.py
@@ -0,0 +1,11 @@
+import os
+
+cnt = 0
+top = os.getcwd()
+for root, dirs, files in os.walk(top, topdown=False):
+    for name in files:
+        #print(os.path.join(root, name))
+        if 'ﬁ' in name:
+            os.rename(os.path.join(root, name), os.path.join(root, name.replace('ﬁ', 'fi')))
+            cnt += 1
+print(cnt, "file(s) changed!")
